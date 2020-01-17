52cd1d448a9cbd80310c2588606f33c5eb74985a
TugboatsGonWild <wesley.mcmillen@blackburn.edu>
2020-01-17 13:53:18 -0600

    done

--- src/hw/pkg0/mcmillen/HW0McMillen.java
+++ src/hw/pkg0/mcmillen/HW0McMillen.java
@@ -5,6 +5,8 @@
  */
 package hw.pkg0.mcmillen;
 
+import java.util.Scanner;
+
 /**
  *
  * @author wesley.mcmillen
@@ -12,10 +14,66 @@ package hw.pkg0.mcmillen;
 public class HW0McMillen {
 
     /**
-     * @param args the command line argumentsssss
+     * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        int Sushi = 1;
+        int Tacos = 2;
+        int Shrimp = 3;
+        String guess1 = " ";
+        String guess2 = " ";
+        String guess3 = " ";
+        String answer = " ";
+        int number = (int) (3 * Math.random()) + 1;
+        if (number == 3) {
+            answer = "Shrimp";
+        }
+        if (number == 2) {
+            answer = "Tacos";
+        }
+        if (number == 1) {
+            answer = "Sushi";
+        }
+        System.out.println("The secret meal must be one of the following");
+        System.out.println("Sushi, Tacos,or Shrimp");
+        System.out.println("Take a guess");
+        guess1 = input.nextLine();
+        if (guess1.equalsIgnoreCase(answer)) {
+            System.out.println("You were right!");
+        } else {
+
+            if (guess1.equalsIgnoreCase("sushi") || guess1.equalsIgnoreCase("tacos") || guess1.equalsIgnoreCase("shrimp")) {
+                System.out.println("Not quite what I had in mind. Try again");
+            } else {
+                System.out.println("That wasn't an option. U lose... loser");
+                return;
+            }
+
+            guess2 = input.nextLine();
+            if (guess2.equalsIgnoreCase(answer)) {
+                System.out.println("You were right!");
+            } else {
+                System.out.println("Still incorrect");
+                if (guess2.equalsIgnoreCase(guess1)) {
+                    System.out.println("Didn't you already try that?");
+                    return;
+                } else if (guess2.equalsIgnoreCase("sushi") || guess2.equalsIgnoreCase("tacos") || guess2.equalsIgnoreCase("shrimp")) {
+                    System.out.println("Try again");
+                }
+                guess3 = input.nextLine();
+                if (guess3.equalsIgnoreCase(answer)) {
+                    System.out.println("You did it");
+                } else {
+                    System.out.println("Still incorrect");
+                    if (guess3.equalsIgnoreCase(guess1) || guess3.equalsIgnoreCase(guess2)) {
+                        System.out.println("Didn't you already try that?");
+                    }
+
+                }
+
+            }
+        }
+
     }
-    
 }
